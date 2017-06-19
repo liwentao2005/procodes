ae60838b71fb3c0b21ac93769d587f3920bf43a8
liwentao <liwentao2005@126.com>
2017-06-19 17:10:07 +0800

    go concurrent test

--- golang/go_example/src/go_example.go
+++ golang/go_example/src/go_example.go
@@ -4,7 +4,27 @@ import (
     "os"
     "fmt"
     "bufio"
+    "math"
+    "time"
 )
+
+type Point struct{X, Y float64}
+
+type Path []Point
+
+func (p Point)Distance(q Point) float64 {
+    return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func (path Path)  Distance() float64 {
+    sum := 0.0
+    for i := range path {
+        if i > 0 {
+            sum += path[i-1].Distance(path[i])
+        }
+    }
+    return sum
+}
 
 func main() {
     counts := make(map[string]int)
@@ -27,6 +47,29 @@ func main() {
             fmt.Printf("%d\t%s\n", n, line)
         }
     }
+
+    f := squares()
+    fmt.Println(f())
+    fmt.Println(f())
+    fmt.Println(f())
+    fmt.Println(f())
+    fmt.Println(f())
+
+    // test path
+    perim := Path{
+        {1, 1},
+        {5, 1},
+        {5, 4},
+        {1, 1},
+    }
+
+    fmt.Println(perim.Distance())
+
+    // goroutine test
+    go spinner(100 * time.Millisecond)
+    const n = 45
+    fibN := fib(n)
+    fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
 }
 
 func countLines(f *os.File, counts map[string]int) {
@@ -34,4 +77,28 @@ func countLines(f *os.File, counts map[string]int) {
     for input.Scan() {
         counts[input.Text()]++
     }
+}
+
+func squares() func() int {
+    var x int
+    return func() int {
+        x++
+        return x * x
+    }
+}
+
+func spinner(delay time.Duration) {
+    for {
+        for _, r := range `-\|/` {
+            fmt.Printf("\r%c%c", r, r)
+            time.Sleep(delay)
+        }
+    }
+}
+
+func fib(x int) int {
+    if x < 2 {
+        return x
+    }
+    return fib(x-1) + fib(x-2)
 }
